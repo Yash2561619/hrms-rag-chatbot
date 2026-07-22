33d659dd8112b237aaf10c376ec2bb12bb27c4d2
Yash Kabure <yashkabure@gmail.com>
2026-07-23 01:16:26 +0530

    Fix Gunicorn logging for Render

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,10 @@
 bind = '0.0.0.0:5000'
+
 workers = 1
 threads = 4
 timeout = 120
-accesslog = 'logs/access.log'
-errorlog = 'logs/error.log'
+
+# Render logging
+accesslog = '-'
+errorlog = '-'
 loglevel = 'info'

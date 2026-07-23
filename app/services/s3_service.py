83b829ac816e8fd546b8c15383920c1c9f3e4d28
Yash Kabure <yashkabure@gmail.com>
2026-07-24 01:05:12 +0530

    Add AWS S3 service for salary storage

---
+++ app/services/s3_service.py
@@ -0,0 +1,37 @@
+import os
+import boto3
+from uuid import uuid4
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
+
+BUCKET = os.getenv('S3_BUCKET_NAME')
+
+def upload_salary_to_s3(file_obj, filename):
+    key = f"salary_slips/{uuid4()}_{filename}"
+
+    s3.upload_fileobj(
+        file_obj,
+        BUCKET,
+        key,
+        ExtraArgs={
+            'ContentType': 'application/pdf'
+        }
+    )
+
+    return key
+
+
+def generate_download_url(key, expires=3600):
+    return s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': BUCKET,
+            'Key': key
+        },
+        ExpiresIn=expires
+    )
